04290d384a64f11dc75a79e4040178baf9df378c
savhsergio <sergioandresherrera@gmail.com>
2023-09-01 18:43:29 -0500

    FastAPI Server Setup

--- server/main.py
+++ server/main.py
@@ -1,4 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request
 import uvicorn
 import os 
 from dotenv import load_dotenv
+
+
+load_dotenv()
+
+api= FastAPI()
+
+@api.get('/test')
+async def root():
+    return {'msg':'API is online'}
+
+if __name__ == '__main__':
+    if os.environ.get('APP_ENV')=='development':
+        uvicorn.run("main:api",host='0.0.0.0', port = 3500, workers=4, reload=True)
+    else:
+        pass
